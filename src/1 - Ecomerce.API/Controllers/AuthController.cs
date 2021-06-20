9331228af6c769a0885d282f97841fa23988d3ad
lucas <lucaslmota0431@gmail.com>
2021-06-20 11:45:01 -0300

    Primeiro programa

---
+++ src/1 - Ecomerce.API/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+using System;
+using Ecomerce.API.Token;
+using Ecomerce.API.Utilities;
+using Ecomerce.API.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Ecomerce.API.Controllers
+{
+    [ApiController]
+    public class AuthController :ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ITokenGenerator _tokenGenerator;
+
+        public AuthController(IConfiguration configuration, ITokenGenerator tokenGenerator)
+        {
+            _configuration = configuration;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost]
+        [Route("/api/v1/auth/login")]
+        public IActionResult Login([FromBody]LoginViewModel loginViewModel){
+            try
+            {
+                var tokenLogin = _configuration["Jwt:Login"];
+                var tokenPassword = _configuration["Jwt:Password"];
+
+                if (loginViewModel.Login == tokenLogin && loginViewModel.Password == tokenPassword)
+                {
+                    return Ok(new ResultadoViewModel
+                    {
+                        Mensagem = "Entidade criada com sucesso!",
+                        Sucesso = true,
+                        Dados = new
+                        {
+                            Token = _tokenGenerator.GenerateToken(),
+                            TokenExpires = DateTime.UtcNow.AddHours(int.Parse(_configuration["Jwt:HoursToExpire"]))
+                        }
+                    });
+                }
+                else
+                {
+                    return StatusCode(401, Responses.UnauthorizedErrorMessage());
+                }
+            }
+            catch(Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+    }
+}
---
+++ src/3 - Ecomerce.Services/DTO/CompanhiaDTO.cs
@@ -0,0 +1,22 @@
+namespace Ecomerce.Services.DTO
+{
+    public class CompanhiaDTO
+    {
+        public CompanhiaDTO()
+        {
+        }
+
+        public CompanhiaDTO(int id, string nomeFantasia, string razaoSocial, string cNPJ)
+        {
+            Id = id;
+            NomeFantasia = nomeFantasia;
+            RazaoSocial = razaoSocial;
+            CNPJ = cNPJ;
+        }
+
+        public int Id { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
